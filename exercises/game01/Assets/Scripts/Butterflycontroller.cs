dc662140c38507a433a61dfd6c9afb51f869b711
schwarzginger <56312265+schwarzginger@users.noreply.github.com>
2020-09-09 19:33:52 -0400

    game01 first commit
    two objects moving

---
+++ exercises/game01/Assets/Scripts/Butterflycontroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterflycontroller : MonoBehaviour
+{
+
+    //public GameObject thingtoChase;
+    //public Transform objectToFollow;
+    //public Vector3 offset;
+
+    public Transform objectToFollow;
+    public float speed = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, objectToFollow.position, step);
+
+
+        if (Vector3.Distance(transform.position, objectToFollow.position) < 0.001f)
+        {
+
+            objectToFollow.position *= -1.0f;
+
+
+
+        }
+
+    }
+
+
+}
+
